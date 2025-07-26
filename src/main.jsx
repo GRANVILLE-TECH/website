4ef75382d31d3bc2213e4b5e160f55b9f0db6023
niquestetia <45465127+niquestetia@users.noreply.github.com>
2025-07-26 14:14:30 +0300

    Wrap app with BrowserRouter and add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import ArticlesPage from './pages/ArticlesPage.jsx'
+import InnovationsPage from './pages/InnovationsPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/articles" element={<ArticlesPage />} />
+        <Route path="/innovations" element={<InnovationsPage />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
